11bb86dd64338080d1fa3ea9819167cd394f500a
657994434 <657994434@qq.com>
2017-01-01 16:21:09 +0800

    给定两个二进制字符串，返回它们的和

--- src/AddBinary.java
+++ src/AddBinary.java
@@ -2,8 +2,8 @@
  * @author 李智
  * @date 2016/12/31
  *
- * Given two binary strings, return their sum (also a binary string).
- * For example,
+ * 给定两个二进制字符串，返回它们的和
+ * 如:
  * a = "11"
  * b = "1"
  * Return "100".
