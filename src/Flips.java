b18f7701e63cba1b22434c7600707b881d47b717
657994434 <657994434@qq.com>
2017-01-04 22:12:27 +0800

    Flips

--- src/Flips.java
+++ src/Flips.java
@@ -2,19 +2,30 @@
 /**
  * @author 李智
  * @date 2017/1/4
- *
+ * <p>
  * 实例化
  */
 public class Flips {
     public static void main(String[] args) {
-        int T = Integer.parseInt(args[0]);
+//        int T = Integer.parseInt(args[0]);
         Counter heads = new Counter("heads");
+        System.out.println(heads.getHead());
 
     }
 }
 
-class Counter{
-    public Counter(String string){
+class Counter {
+    private String head;
 
+    public Counter(String head) {
+        this.head = head;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public void setHead(String head) {
+        this.head = head;
     }
 }
