af3b0b33afe255e226c801de255e59ce76a41203
657994434 <657994434@qq.com>
2017-01-08 19:10:59 +0800

    给定一个二叉树，确定它是否是高度平衡的。

---
+++ src/BalancedBinaryTree.java
@@ -0,0 +1,49 @@
+/**
+ * @author 李智
+ * @date 2017/1/8
+ * <p>
+ * Given a binary tree, determine if it is height-balanced.
+ * For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees
+ * of every node never differ by more than 1.
+ */
+public class BalancedBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null)
+            return true;
+        boolean left = isBalanced(root.left);
+        boolean right = isBalanced(root.right);
+        if (left && right && Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1)
+            return true;
+        return false;
+    }
+
+    public int getHeight(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        return Math.max(left, right) + 1;
+
+    }
+}
+
+class TreeNode {
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRoght() {
+        return right;
+    }
+
+    public void setRoght(TreeNode right) {
+        this.right = right;
+    }
+}
