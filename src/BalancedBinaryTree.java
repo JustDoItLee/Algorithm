8f85f9f0f28f6c879a83f5037397f01c976b9f53
657994434 <657994434@qq.com>
2017-01-12 18:00:39 +0800

    TreeNode

--- src/BalancedBinaryTree.java
+++ src/BalancedBinaryTree.java
@@ -30,6 +30,15 @@ public class BalancedBinaryTree {
 class TreeNode {
     public TreeNode left;
     public TreeNode right;
+    public Integer val;
+
+    public Integer getVal() {
+        return val;
+    }
+
+    public void setVal(Integer val) {
+        this.val = val;
+    }
 
     public TreeNode getLeft() {
         return left;
