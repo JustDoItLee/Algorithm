8cf15fff30ded90c38b46f8f92a8cd466a857515
657994434 <657994434@qq.com>
2017-01-12 18:00:29 +0800

    在二叉搜索树上实现迭代器

--- src/BinarySearchTreeIterator.java
+++ src/BinarySearchTreeIterator.java
@@ -1,3 +1,6 @@
+import java.util.Iterator;
+import java.util.Stack;
+
 /**
  * @author 李智
  * @date 2017/1/12
@@ -6,6 +9,34 @@
  * Calling next() will return the next smallest number in the BST.
  * Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
  */
-public class BinarySearchTreeIterator {
+public class BinarySearchTreeIterator implements Iterator<Integer> {
+    public Stack<TreeNode> stack = new Stack<TreeNode>();
 
+    public BinarySearchTreeIterator(TreeNode root) {
+        pushAll(root);
+    }
+
+    /**
+     * @return whether we have a next smallest number
+     */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /**
+     * @return the next smallest number
+     */
+    public Integer next() {
+        TreeNode temp = stack.pop();
+        pushAll(temp.right);
+        return temp.val;
+    }
+
+    public void pushAll(TreeNode root) {
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
 }
+
