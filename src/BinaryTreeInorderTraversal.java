5ef8ecd7b2fa93d81800eb8c0fec931b27f6e5d1
657994434 <657994434@qq.com>
2017-01-13 17:42:52 +0800

    给定一个二叉树，返回其节点值的有序遍历。

---
+++ src/BinaryTreeInorderTraversal.java
@@ -0,0 +1,30 @@
+/**
+ * @author 李智
+ * @date 2017/1/13
+ *
+ * Given a binary tree, return the in-order traversal of its nodes' values.
+ * For example:
+ * Given binary tree {1,#,2,3},
+ *    1
+ *      \
+ *        2
+ *      /
+ *    3
+ * return [1,3,2].
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ * OJ's Binary Tree Serialization:
+ * The serialization of a binary tree follows a level order traversal, where '#' signifies a path terminator
+ * where no node exists below.
+ * Here's an example:
+ *   1
+ *  / \
+ * 2   3
+ *    /
+ *   4
+ *    \
+ *     5
+ * The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
+ */
+public class BinaryTreeInorderTraversal {
+
+}
