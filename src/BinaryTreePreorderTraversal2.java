81c312ed3ffeb8bec56245f10bafc8aaf44112a1
657994434 <657994434@qq.com>
2017-01-20 18:02:11 +0800

    给定一个二叉树，返回其节点值的前序遍历。

--- src/BinaryTreePreorderTraversal2.java
+++ src/BinaryTreePreorderTraversal2.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author 李智
  * @date 2017/1/20
@@ -14,4 +17,19 @@
  * Note: Recursive solution is trivial, could you do it iteratively?
  */
 public class BinaryTreePreorderTraversal2 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        if (root == null)
+            return list;
+        helper(list, root);
+        return list;
+    }
+
+    public void helper(ArrayList<Integer> list, TreeNode node) {
+        if (node != null) {
+            list.add(node.val);
+            helper(list, node.left);
+            helper(list, node.right);
+        }
+    }
 }
