4b9608554c6c57043fa154a22df75b65e5345452
657994434 <657994434@qq.com>
2017-01-21 16:35:41 +0800

    给定一个二叉树，确定它是否是高度平衡的。

--- src/BalancedBinaryTree.java
+++ src/BalancedBinaryTree.java
@@ -26,33 +26,3 @@ public class BalancedBinaryTree {
 
     }
 }
-
-class TreeNode {
-    public TreeNode left;
-    public TreeNode right;
-    public Integer val;
-
-    public Integer getVal() {
-        return val;
-    }
-
-    public void setVal(Integer val) {
-        this.val = val;
-    }
-
-    public TreeNode getLeft() {
-        return left;
-    }
-
-    public void setLeft(TreeNode left) {
-        this.left = left;
-    }
-
-    public TreeNode getRoght() {
-        return right;
-    }
-
-    public void setRoght(TreeNode right) {
-        this.right = right;
-    }
-}
