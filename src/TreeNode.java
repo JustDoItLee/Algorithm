2a7453959834ccc87a9a7cfa0a46cd07addb122e
657994434 <657994434@qq.com>
2017-01-21 16:35:25 +0800

    TreeNode

---
+++ src/TreeNode.java
@@ -0,0 +1,33 @@
+/**
+ * @author 李智
+ * @date 2017/1/21
+ */
+public class TreeNode {
+    public TreeNode left;
+    public TreeNode right;
+    public Integer val;
+
+    public Integer getVal() {
+        return val;
+    }
+
+    public void setVal(Integer val) {
+        this.val = val;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRoght() {
+        return right;
+    }
+
+    public void setRoght(TreeNode right) {
+        this.right = right;
+    }
+}
