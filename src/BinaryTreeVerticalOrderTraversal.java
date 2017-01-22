d22369d217da1adf7f9eccc3cfd08c1553846805
657994434 <657994434@qq.com>
2017-01-22 16:02:18 +0800

    每一列给它一个列号，左孩子比父节点的列号减1，右孩子比父节点的加1.列号相同的打印在同一行

--- src/BinaryTreeVerticalOrderTraversal.java
+++ src/BinaryTreeVerticalOrderTraversal.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * @author 李智
  * @date 2017/1/22
@@ -21,4 +23,32 @@
  *  这里垂直访问的意思是，每一列给它一个列号，左孩子比父节点的列号减1，右孩子比父节点的加1.列号相同的打印在同一行。
  */
 public class BinaryTreeVerticalOrderTraversal {
+    public void printVerticalOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+        int leftlen = 0;
+        while (root != null) {
+            root = root.left;
+            leftlen++;
+        }
+        helper(list, root, 0, leftlen);
+        for (int i = 0; i < list.size(); i++) {
+            ArrayList<Integer> l = list.get(i);
+            for (int j = 0; j < l.size(); j++) {
+                System.out.print(l.get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private void helper(ArrayList<ArrayList<Integer>> list, TreeNode root, int index, int leftlen) {
+        if (root == null)
+            return;
+        if (list.get(index + leftlen) != null) {
+            list.get(index + leftlen).add(root.val);
+        } else {
+            list.add(new ArrayList<>(root.val));
+        }
+        helper(list, root.left, index - 1, leftlen);
+        helper(list, root.right, index + 1, leftlen);
+    }
 }
