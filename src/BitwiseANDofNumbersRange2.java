ef02a616e04c40f2e72337ba068061337194e0df
657994434 <657994434@qq.com>
2017-02-14 17:06:02 +0800

    给定一个范围[m，n]，其中0 <= m <= n <= 2147483647，返回该范围内所有数字

--- src/BitwiseANDofNumbersRange2.java
+++ src/BitwiseANDofNumbersRange2.java
@@ -5,4 +5,30 @@
  * For example, given the range [5, 7], you should return 4.
  */
 public class BitwiseANDofNumbersRange2 {
+    public static int rangeBitwiseAnd(int m, int n) {
+        int[] bit = new int[32];
+        int res = 0;
+        for (int i = m; i <= n; i++) {
+            int cur = i;
+            while (cur > 0) {
+                int base = 2;
+                int count = 0;
+                while (base <= cur) {
+                    count++;
+                    base *= 2;
+                }
+                bit[count]++;
+                cur = cur - base / 2;
+            }
+        }
+        for (int i = 0; i < 32; i++) {
+            if (bit[i] == n - m + 1)
+                res += Math.pow(2, i);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(rangeBitwiseAnd(10, 13));
+    }
 }
