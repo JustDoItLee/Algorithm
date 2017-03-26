37764b61647ff5b3080bcce318a26737e47e195b
657994434 <657994434@qq.com>
2017-03-26 10:59:28 +0800

    糖果问题

---
+++ src/Candy.java
@@ -0,0 +1,12 @@
+/**
+ * @author 李智
+ * @date 2017/3/26
+ *
+ * There are N children standing in a line. Each child is assigned a rating
+ * You are giving candies to these children subjected to the following
+ * Each child must have at least one candy.
+ * Children with a higher rating get more candies than their neighbors.
+ * What is the minimum candies you must give?
+ */
+public class Candy {
+}
