658ed4b164084a6a4bff47400d6c49a910bde91b
657994434 <657994434@qq.com>
2017-03-29 09:34:12 +0800

    爬楼梯

---
+++ src/ClimbingStairs.java
@@ -0,0 +1,8 @@
+/**
+ * @author 李智
+ * @date 2017/3/29
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ */
+public class ClimbingStairs {
+}
