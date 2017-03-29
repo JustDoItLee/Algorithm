bd7537c941f8e67359937eefa68bed59767ed812
657994434 <657994434@qq.com>
2017-03-29 09:34:31 +0800

    爬楼梯

--- src/ClimbingStairs.java
+++ src/ClimbingStairs.java
@@ -5,4 +5,9 @@
  * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
  */
 public class ClimbingStairs {
+    public int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
 }
