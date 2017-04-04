cebb953d8199906d0a582f33ed3a9dfa5b1e095e
657994434 <657994434@qq.com>
2017-04-04 15:55:45 +0800

    克隆无向图

--- src/CloneGraph.java
+++ src/CloneGraph.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author 李智
  * @date 2017/4/4
@@ -21,4 +26,42 @@
  * Solution 1 BFS
  */
 public class CloneGraph {
+    class UndirectedGraphNode {
+        int label;
+        List<UndirectedGraphNode> neighbors;
+
+        UndirectedGraphNode(int x) {
+            label = x;
+            neighbors = new ArrayList<UndirectedGraphNode>();
+        }
+    }
+
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if (node == null)
+            return null;
+
+        LinkedList<UndirectedGraphNode> queue = new LinkedList<UndirectedGraphNode>();
+        HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
+
+        UndirectedGraphNode newHead = new UndirectedGraphNode(node.label);
+        queue.add(node);
+        map.put(node, newHead);
+
+        while (!queue.isEmpty()) {
+            UndirectedGraphNode curr = queue.pop();
+            List<UndirectedGraphNode> currNeighbors = curr.neighbors;
+            for (UndirectedGraphNode aNeighbor : currNeighbors) {
+                if (!map.containsKey(aNeighbor)) {
+                    UndirectedGraphNode copy = new UndirectedGraphNode(aNeighbor.label);
+                    map.put(aNeighbor, copy);
+                    map.get(curr).neighbors.add(copy);
+                    queue.add(aNeighbor);
+                } else {
+                    map.get(curr).neighbors.add(map.get(aNeighbor));
+                }
+            }
+
+        }
+        return newHead;
+    }
 }
