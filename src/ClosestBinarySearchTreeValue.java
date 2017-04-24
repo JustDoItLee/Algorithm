e91a9ef56fab764df3eba42a9a0753e54e1e9aff
657994434 <657994434@qq.com>
2017-04-24 09:01:43 +0800

    二叉树查找值

---
+++ src/ClosestBinarySearchTreeValue.java
@@ -0,0 +1,11 @@
+/**
+ * @author 李智
+ * @date 2017/4/24
+ *
+ * Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
+ * Note:
+ * Given target value is a floating point.
+ * You are guaranteed to have only one unique value in the BST that is closest to the target.
+ */
+public class ClosestBinarySearchTreeValue {
+}
