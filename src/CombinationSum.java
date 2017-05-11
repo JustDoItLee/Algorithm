2db4c8828c9b4e063dd8f8aa48cfddce9ae5e3a0
657994434 <657994434@qq.com>
2017-05-11 14:50:17 +0800

    给定一组候选数和目标数，找到所有独特组合

--- src/CombinationSum.java
+++ src/CombinationSum.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @author 李智
  * @date 2017/5/11
@@ -15,5 +18,29 @@
  * [2, 2, 3]
  */
 public class CombinationSum {
+    public ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        if (candidates == null || candidates.length == 0)
+            return res;
+        Arrays.sort(candidates);
+        helper(candidates, 0, target, new ArrayList<Integer>(), res);
+        return res;
+    }
 
+    private void helper(int[] candidates, int start, int target, ArrayList<Integer> item,
+                        ArrayList<ArrayList<Integer>> res) {
+        if (target < 0)
+            return;
+        if (target == 0) {
+            res.add(new ArrayList<Integer>(item));
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            if (i > 0 && candidates[i] == candidates[i - 1])
+                continue;
+            item.add(candidates[i]);
+            helper(candidates, i, target - candidates[i], item, res);
+            item.remove(item.size() - 1);
+        }
+    }
 }
